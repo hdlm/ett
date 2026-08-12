4239d00b9326af57c389fe7bf5f0781e7d74bff7
Henry De la Mano <hdelamano@gmail.com>
2026-08-12 03:39:10 -0400

    preparing the release v0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
     defaultConfig {
         applicationId = "com.budoxr.ett"
         minSdk = AndroidSdk.MIN
-        versionCode = 3
-        versionName = "0.3"
+        versionCode = 4
+        versionName = "0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
